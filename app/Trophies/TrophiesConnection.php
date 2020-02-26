113364a8e833d8911250f4b9b9380b35ff5db3dc
David Peach <davidpeach@Davids-MacBook-Pro.local>
2020-02-26 00:13:39 +0000

    UPDATE grabbing all my games ready to loop through.

--- app/Trophies/TrophiesConnection.php
+++ app/Trophies/TrophiesConnection.php
@@ -6,16 +6,20 @@ use App\Trophies\Auth;
 
 class TrophiesConnection
 {
+	private $psn;
+
 	public function __construct(Auth $auth)
 	{
-		$this->auth = $auth;
-
-		$this->auth->reconnect();
+		$this->psn = $auth->reconnect()->getConnection();
 	}
 
 	public function import()
 	{
-		$psn = $this->auth->getConnection();
-		//
+		$games = $this->psn->user()->games();
+
+		foreach ($games as $game) {
+			// Up to importing game trophies
+		}
+
 	}
 }
